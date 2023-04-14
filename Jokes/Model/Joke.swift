ffb6612930669458bc361efc75115b6bd59a9305
Alistair Fraser <afraser@lcs.on.ca>
2023-04-14 14:41:57 -0400

    Added a model to match the structure of the JSON object received from our endpoint.

---
+++ Jokes/Model/Joke.swift
@@ -0,0 +1,16 @@
+//
+//  Joke.swift
+//  Jokes
+//
+//  Created by Alistair Fraser on 2023-04-14.
+//
+
+import Foundation
+
+struct Joke: Identifiable {
+    let type: String
+    let setup: String
+    let punchline: String
+    let id: Int
+}
+let exampleJoke = Joke(type: "general", setup: "How much does a hipster weigh?", punchline: "An Instagram", id: 146)
