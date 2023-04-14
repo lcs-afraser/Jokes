3ec499c49e6a7077df67d3b3a814a0c1e4fbed46
Alistair Fraser <afraser@lcs.on.ca>
2023-04-14 15:18:52 -0400

    Finished the app

--- Jokes/Views/JokeView.swift
+++ Jokes/Views/JokeView.swift
@@ -22,6 +22,8 @@ struct JokeView: View {
         NavigationView {
             VStack {
                 
+                Spacer()
+                
                 if let currentJoke = currentJoke {
                     //Show the joke if it can be unwrapped(if currentJoke is not nil
                     Text(currentJoke.setup)
@@ -49,7 +51,23 @@ struct JokeView: View {
                   ProgressView()
                 }
                 
-                
+                Spacer()
+                Button(action: {
+                    // Reset the interface
+                    punchlineOpacity = 0.0
+
+                    Task {
+                        // Get another joke
+                        withAnimation {
+                            currentJoke = nil
+                        }
+                        currentJoke = await NetworkService.fetch()
+                    }
+                }, label: {
+                    Text("Fetch another one")
+                })
+                .disabled(punchlineOpacity == 0.0 ? true : false)
+                .buttonStyle(.borderedProminent)
             }
             .navigationTitle("Random Jokes")
         }
